08b48e2af603ee52992fde5010ab877f38558abc
IBN5100X <13821904999@163.com>
2026-05-03 17:35:14 +0800

    Create focuswindow.h

---
+++ focuswindow.h
@@ -0,0 +1,47 @@
+#ifndef FOCUSWINDOW_H
+#define FOCUSWINDOW_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QTimer>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QKeyEvent>
+
+class FocusWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    FocusWindow(QWidget* parent = nullptr);
+
+    void setTotalSeconds(int seconds);
+    void startCountdown();
+
+public:
+    void keyPressEvent(QKeyEvent *event) override;
+
+signals:
+    void countdownFinished(int focusedSeconds);
+    void countdownStopped(int focusedSeconds);
+
+public slots:
+    void updateCountdown();
+    void switchPause();
+    void endEarly();
+
+public:
+    QLabel* timeShow;
+    QPushButton* pauseButton;
+    QPushButton* endButton;
+    QTimer* timer;
+
+    int totalSeconds;
+    int remainingSeconds;
+    bool isPaused;
+
+    void updateTimeDisplay();
+};
+
+#endif // FOCUSWINDOW_H
